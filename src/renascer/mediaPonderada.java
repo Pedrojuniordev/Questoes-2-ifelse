824d360e47f9f6035e385eabb383a13f0bbb4eaa
Pedrojuniordev <pedrojunior44.pj@gmail.com>
2023-09-10 00:31:25 +0100

    check questao media pondera

--- src/renascer/mediaPonderada.java
+++ src/renascer/mediaPonderada.java
@@ -1,9 +1,29 @@
 package renascer;
 
+import java.util.Scanner;
 
 public class mediaPonderada {
 
-	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int peso1 = 1;
+		int peso2 = 2;
+		int peso3 = 3;
+		int nota1, nota2, nota3, medponderada;
+		
+		System.out.println("Digite Nota 1:" );
+		nota1 = scanner.nextInt();
+		System.out.println("Digite Nota 2:" );
+		nota2 = scanner.nextInt();
+		System.out.println("Digite Nota 3:" );
+		nota3 = scanner.nextInt();
+		
+		medponderada = ((nota1*peso1)+(nota2*peso2)+(nota3*peso3))/(peso1+peso2+peso3);
+		
+		System.out.printf("A média pondera é:  %.3f" + medponderada);
+		
+	}
 	
 
 }
