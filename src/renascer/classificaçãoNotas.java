217d3445b7c9db2383e09f0728d69e86981a39ac
Pedrojuniordev <pedrojunior44.pj@gmail.com>
2023-09-16 00:27:44 +0100

    verificadorVotacao

---
+++ src/renascer/classificaçãoNotas.java
@@ -0,0 +1,13 @@
+package renascer;
+
+import java.util.Scanner;
+
+public class classificaçãoNotas {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Digite uma nota entre 0 a 100");
+			
+	}
+}
---
+++ src/renascer/diaDaSemana.java
@@ -0,0 +1,14 @@
+package renascer;
+
+import java.util.Scanner;
+
+public class diaDaSemana {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		
+		System.out.println("Digite número de 1 a 7");
+	}
+
+}
